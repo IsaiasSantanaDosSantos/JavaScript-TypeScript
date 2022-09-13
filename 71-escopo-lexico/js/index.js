ee456bceceda01fbfcb8aa49a58c7d1ece1eb11d
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-09-12 21:04:49 -0300

    escopo léxico

---
+++ 71-escopo-lexico/js/index.js
@@ -0,0 +1,11 @@
+const nome = 'Luiz';
+function falaNome() {
+  //   const nome = 'Isaias';
+  console.log(nome);
+}
+
+function usaFalaNome() {
+  const nome = 'Isaias';
+  falaNome();
+}
+usaFalaNome();
