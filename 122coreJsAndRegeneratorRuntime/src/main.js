89c2f249602b682a1e91e1d9d6e87f3af2ebad5f
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-11-14 20:03:53 -0300

    corrigindo erro em navegadores mais antigos

---
+++ 122coreJsAndRegeneratorRuntime/src/main.js
@@ -0,0 +1,6 @@
+import 'core-js/stable' //Para garantir funcionmento em navegadores mais antigos
+import "regenerator-runtime"; //Para garantir funcionmento em navegadores mais antigos
+import executa from './modules/promises';
+import './assets/css/style.css';
+
+executa();
---
+++ 122coreJsAndRegeneratorRuntime/src/modules/promises.js
@@ -0,0 +1,16 @@
+// import { reject, resolve } from "core-js/fn/promise";
+// import { async } from "regenerator-runtime";
+
+function promise(){
+return new Promise((resolve, reject) => {
+    setTimeout(()=> {
+console.log("Hey, sou a promise.")
+resolve()
+    }, 2000)
+})
+}
+
+ export default async function ()  {
+    await promise()
+    console.log("Terminou")
+}
