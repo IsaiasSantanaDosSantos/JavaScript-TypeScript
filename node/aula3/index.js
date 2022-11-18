1dc03570cdc4340872189b86aab66202774e5d51
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-11-17 21:46:35 -0300

    node commands

---
+++ node/aula3/index.js
@@ -0,0 +1,45 @@
+/**Comandos:
+ * Ir para a pasta:
+ * cd aula3
+ *
+ * Iniciar node mode resumido:
+ * npm init -y
+ *
+ * Instalar pacote:
+ *  npm install express
+ *
+ * Desinstalar pacotes:
+ *  npm uninstall express webpack
+ *
+ * Verificar de há pacotes para atualizar:
+ *  npm outdated
+ *
+ * Instalar um versão com o "major" específico:
+ *  npm i express@4.x
+ *
+ * Instalar uma versão específica:
+ *  npm i express@2.1.1
+ *
+ * Atualizar pacotes:
+ *  npm update
+ *
+ * Listar instalações:
+ * npm ls
+ *
+ * Listar instalações por camadas/profundidade:
+ * npm ls --depth=0
+ *
+ * Instalar/mudar pacote de produção para dev dependência:
+ * npm i webpack --save-dev
+ *
+ * Instalar/mudar pacote de dev dependência para produção:
+ * npm i express --save-prod
+ *
+ * Instala sem o "^" para não atualizar:
+ * npm install express -E
+ *
+ * Retira o "^" para não atualizar:
+ * npm express -E
+ *
+ *
+ */
