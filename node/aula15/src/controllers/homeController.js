f3a87e13a0cc2b23f25adc4f01c3b79ed6336d2b
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2022-11-29 21:49:54 -0300

    Express Session and Flash Messeges

---
+++ node/aula15/src/controllers/homeController.js
@@ -0,0 +1,12 @@
+exports.paginaInicial = (req, res) => {
+  console.log(req.session.usuario);
+  console.log(req.flash('info'),req.flash('error'), req.flash('success') )
+  
+  res.render('index');
+  return;
+};
+
+exports.trataPost = (req, res) => {
+  res.send(req.body);
+  return;
+};
